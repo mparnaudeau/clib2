1b9f5541af660a3983fd8ba3050f612c002ca18b
Ola Söder <rolfkopman@gmail.com>
2021-01-29 23:18:12 +0100

    Add wcrtomb.

--- library/wchar_wcrtomb.c
+++ library/wchar_wcrtomb.c
@@ -1,34 +1,5 @@
 /*
- * $Id: wchar_wcrtomb.c,v 1.3 2006-01-08 12:04:27 obarthel Exp $
- *
- * :ts=4
- *
- * Portable ISO 'C' (1994) runtime library for the Amiga computer
- * Copyright (c) 2002-2015 by Olaf Barthel <obarthel (at) gmx.net>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Neither the name of Olaf Barthel nor the names of contributors
- *     may be used to endorse or promote products derived from this
- *     software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * FIXME: From MUSL.
  */
 
 #ifndef _WCHAR_HEADERS_H
@@ -40,6 +11,53 @@
 size_t
 wcrtomb(char *s, wchar_t wc, mbstate_t *ps)
 {
-	/* ZZZ unimplemented */
-	return(0);
+    if(!s)
+    {
+        return 1;
+    }
+
+    if((unsigned) wc < 0x80)
+    {
+        *s = wc;
+        return 1;
+    }
+
+    if(MB_CUR_MAX == 1)
+    {
+        if (!IS_CODEUNIT(wc))
+        {
+            errno = EILSEQ;
+            return -1;
+        }
+
+        *s = wc;
+        return 1;
+    }
+
+    if((unsigned) wc < 0x800)
+    {
+        *s++ = 0xc0 | (wc>>6);
+        *s = 0x80 | (wc&0x3f);
+        return 2;
+    }
+
+    if((unsigned) wc < 0xd800 || (unsigned) wc - 0xe000 < 0x2000)
+    {
+        *s++ = 0xe0 | (wc>>12);
+        *s++ = 0x80 | ((wc>>6)&0x3f);
+        *s = 0x80 | (wc&0x3f);
+        return 3;
+    }
+
+    if((unsigned) wc - 0x10000 < 0x100000)
+    {
+        *s++ = 0xf0 | (wc>>18);
+        *s++ = 0x80 | ((wc>>12)&0x3f);
+        *s++ = 0x80 | ((wc>>6)&0x3f);
+        *s = 0x80 | (wc&0x3f);
+        return 4;
+    }
+
+    errno = EILSEQ;
+    return -1;
 }
