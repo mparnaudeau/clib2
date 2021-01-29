42c75d2868585abf1a3970ac98deb284c8146d0b
Ola Söder <rolfkopman@gmail.com>
2021-01-29 23:39:36 +0100

    Add wcsrtombs.

--- library/wchar_wcsrtombs.c
+++ library/wchar_wcsrtombs.c
@@ -1,34 +1,5 @@
 /*
- * $Id: wchar_wcsrtombs.c,v 1.3 2006-01-08 12:04:27 obarthel Exp $
- *
- * :ts=4
- *
- * Portable ISO 'C' (1994) runtime library for the Amiga computer
- * Copyright (c) 2002-2015 by Olaf Barthel <obarthel (at) gmx.net>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Neither the name of Olaf Barthel nor the names of contributors
- *     may be used to endorse or promote products derived from this
- *     software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * FIXME: From MUSL
  */
 
 #ifndef _WCHAR_HEADERS_H
@@ -38,8 +9,100 @@
 /****************************************************************************/
 
 size_t
-wcsrtombs(char *s, const wchar_t **src, size_t n, mbstate_t *ps)
+wcsrtombs(char *s, const wchar_t **ws, size_t n, mbstate_t *st)
 {
-	/* ZZZ unimplemented */
-	return(0);
+    const wchar_t *ws2;
+    char buf[4];
+    size_t N = n, l;
+
+    if(!s)
+    {
+        for(n=0, ws2 = *ws; *ws2; ws2++)
+        {
+            if(*ws2 >= 0x80u)
+            {
+                l = wcrtomb(buf, *ws2, 0);
+
+                if(!(l + 1))
+                {
+                    return -1;
+                }
+
+                n += l;
+            }
+            else
+            {
+                n++;
+            }
+        }
+        return n;
+    }
+
+    while(n >= 4)
+    {
+        if(**ws - 1u >= 0x7fu)
+        {
+            if(!**ws)
+            {
+                *s = 0;
+                *ws = 0;
+                return N - n;
+            }
+
+            l = wcrtomb(s, **ws, 0);
+
+            if(!(l + 1))
+            {
+                return -1;
+            }
+
+            s += l;
+            n -= l;
+        }
+        else
+        {
+            *s++ = **ws;
+            n--;
+        }
+
+        (*ws)++;
+    }
+
+    while(n)
+    {
+        if(**ws - 1u >= 0x7fu)
+        {
+            if(!**ws)
+            {
+                *s = 0;
+                *ws = 0;
+                return N-n;
+            }
+
+            l = wcrtomb(buf, **ws, 0);
+
+            if(!(l + 1))
+            {
+                return -1;
+            }
+
+            if(l > n)
+            {
+                return N-n;
+            }
+
+            wcrtomb(s, **ws, 0);
+            s += l;
+            n -= l;
+        }
+        else
+        {
+            *s++ = **ws;
+            n--;
+        }
+
+        (*ws)++;
+    }
+
+    return N;
 }
